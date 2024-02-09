ce6d7179ca70169a3e4b6dc7cc27cb9063b1f851
jcruzcruz35 <jcruzcruz35@gmail.com>
2024-02-09 17:37:45 -0600

    primer scrip

---
+++ script.js
@@ -0,0 +1,101 @@
+var botonEncriptar = document.querySelector(".botonEncriptar");
+var botonDesencriptar = document.querySelector(".botonDesencriptar");
+var personaje = document.querySelector(".imgPersonaje");
+var contenedor = document.querySelector(".mensajeEncriptado");
+var resultado = document.querySelector(".resultado");
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+
+function encriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = encriptarTexto(cajatexto);
+}
+
+function desencriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = desencriptarTexto(cajatexto);
+}
+
+function recuperarTexto(){
+    var cajatexto = document.querySelector(".areaTexto")
+    return cajatexto.value
+}
+
+function ocultarAdelante(){
+    personaje.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+
+}
+
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i];
+        }
+        
+    }
+
+    return textoFinal;
+
+}
+
+const btnCopiar = document.querySelector(".botonCopiar"); 
+    botonCopiar.addEventListener("click", copiar = () => {
+    var contenido = document.querySelector(".resultado").textContent;
+    navigator.clipboard.writeText(contenido);
+    console.log("hola"); 
+});
